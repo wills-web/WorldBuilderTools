f6fcd736b6f791a336294b65b619bacc076ccb04
Will Mulrooney <willmulrooney@gmail.com>
2020-05-30 15:54:30 +0100

    init

---
+++ public/UniformGenerator/js/UniformGenny.js
@@ -0,0 +1,16 @@
+// Reference the color shape that was drawn over the image
+const overlay = document.getElementById("product-shape");
+
+// Click on a color
+
+var el = document.getElementsByClassName("color");
+for (var i = 0; i < el.length; i++) {
+  el[i].onclick = changeColor;
+}
+
+function changeColor(e) {
+  // get the hex color
+  let hex = e.target.getAttribute("data-hex");
+  // set the hex color
+  overlay.style.fill = hex;
+}
